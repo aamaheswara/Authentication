0ba54dc263b1c8cf263e1daf416a959d9e9e8f5b
aamaheswara <39681286+aamaheswara@users.noreply.github.com>
2021-12-22 02:13:12 +0700

    logout berhasil, bottom navbar ngebug

--- app/src/main/java/com/android/authentication/ProfileFragment.kt
+++ app/src/main/java/com/android/authentication/ProfileFragment.kt
@@ -1,47 +1,43 @@
 package com.android.authentication
 
+import android.app.ActionBar
 import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.android.authentication.databinding.ActivityHomeBinding
+import com.android.authentication.databinding.FragmentProfileBinding
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.*
 
-
-/*// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-*
- * A simple [Fragment] subclass.
- * Use the [ProfileFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ProfileFragment : Fragment() {
-    /* TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }*/
-
     private lateinit var profileHomeBinding: ActivityHomeBinding
+    private lateinit var profileBinder: FragmentProfileBinding
+    private lateinit var actionBar: ActionBar
+    private lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+//        actionBar = supportActionBar!!
+//        actionBar.title = "ProfileFragment"
+        profileBinder = FragmentProfileBinding.inflate(layoutInflater)
+        activity?.setContentView(profileBinder.root)
+        firebaseAuth = FirebaseAuth.getInstance()
+        checkUser()
 
+        profileBinder.btnLogout.setOnClickListener{
+            firebaseAuth.signOut()
+            checkUser()
+        }
+
+        return inflater.inflate(R.layout.fragment_profile, container, false)
 //        profileHomeBinding.btnLogout.setOnClickListener {
 //            Firebase.auth.signOut()
 //            val intent = Intent(this, LoginActivity::class.java)
@@ -50,23 +46,16 @@ class ProfileFragment : Fragment() {
 //        }
     }
 
-    /*companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ProfileFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ProfileFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }*/
+    private fun checkUser() {
+        // to check is user logged in or not????
+        val firebaseUser = firebaseAuth.currentUser
+        if (firebaseUser != null){
+            val email: String = firebaseUser.email.toString()
+            profileBinder.etEmail.setText(email)
+        } else {
+            startActivity(Intent(activity, LoginActivity::class.java))
+            activity?.finish()
+        }
+    }
+
 }
